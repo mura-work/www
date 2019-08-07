c48fba99f0f8311b97e066cf9ccc2a465988cb10
murata <murata.naoya.infratop@gmail.com>
2019-08-07 23:23:59 +0900

    select_box修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,14 +6,11 @@ class Book < ApplicationRecord
 	has_many :favorites, dependent: :destroy
 	has_many :favorited_users, through: :favorites, source: :user
 
-
 	validates :person_name, presence:true
 	validates :body, presence:true
 	validates :tag_list, presence:true
 
 	default_scope { order(favorites_count: :desc,created_at: :DESC) }
-
-
 
 	acts_as_taggable_on :labels
 	acts_as_taggable
